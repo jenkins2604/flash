f7e00d43d4fa8abdaf66c741584300ac858f43a3
thoang <thiquynhchi.hoang@ctek.com>
2022-12-06 10:33:04 +0100

    aa

--- CC2/checkCS.sh
+++ CC2/checkCS.sh
@@ -2,7 +2,7 @@
 while true
 do
     re=$(echo quit|nc -q 0 localhost 8001)
-    if [ $? -ne 0 ]; then exit 1
+    if [ $? -ne 0 ]; then exit 2; fi
     err=$(echo $re | cut -c 1-7)
     if [[ $err == "ErrorCS" ]] || [[ $err == "" ]]
     then sleep 20s; continue
