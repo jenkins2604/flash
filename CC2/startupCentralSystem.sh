0d703d50ea2af2fa403b6fb6e204e8dccb430aa4
thoang <thiquynhchi.hoang@ctek.com>
2022-12-28 16:32:28 +0100

    update 13

--- CC2/startupCentralSystem.sh
+++ CC2/startupCentralSystem.sh
@@ -3,7 +3,7 @@ curl "http://192.168.17.123/current_state.json?pw=admin&Relay4=0" && sleep 3s
 curl "http://192.168.17.123/current_state.json?pw=admin&Relay4=1&Relay13=0&Relay14=0&Relay5=0&Relay6=0&"\
 "Relay11=0&Relay12=0&Relay7=0&Relay8=0&Relay2=1&Relay15=1"
 sleep 5s
-url = "http://192.168.17.123/current_state.json?pw=admin&Relay2=0&Relay15=0"
+curl "http://192.168.17.123/current_state.json?pw=admin&Relay2=0&Relay15=0"
 sleep 50s
 python3 CC2/CentralSystem.py &
 sleep 10s
