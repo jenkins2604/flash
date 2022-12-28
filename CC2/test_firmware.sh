5976930e5df82fa4652fa2635ef38f52eb7c86a2
thoang <thiquynhchi.hoang@ctek.com>
2022-12-28 12:40:49 +0100

    update 7

--- CC2/test_firmware.sh
+++ CC2/test_firmware.sh
@@ -10,23 +10,24 @@ do
     else
         err1=$(echo $re|jq -r '.message.notificationStatus[1].error_code')
         err2=$(echo $re|jq -r '.message.notificationStatus[2].error_code')
+        echo 'check error connector 1 ${err1}'
+        echo 'check error connector 2 ${err2}'
         if [[ $err1 != "NoError" ]] && [[ $err2 != "NoError" ]]
             then
-                echo "Test station faulty"
+                echo "Test station faulty. Fix the issues and try again."
                 exit 1
         fi
-        re=$(echo $re|jq -r '.message.updatestatus')
-        echo $re
+        re=$(echo $re|jq -r '.message.updateStatus')
     fi
-
-    if [[ $re == "Installed" ]]
+    echo $re
+    if [[ $re == "null" ]] || [[ $re == "NA" ]] || [[ $re == "DownloadFailed" ]] || [[ $re == "InstallationFailed" ]]
+    then
+        echo Failed
+        exit 1
+    elif [[ $re == "Installed" ]]
     then
         echo reboot|nc -w 10 localhost 8001
         echo Installed
         exit 0
-    elif [[ $re == "NA" ]] || [[ $re == "DownloadFailed" ]] || [[ $re == "InstallationFailed" ]]
-    then
-        echo Failed
-        exit 1
     fi
 done
