4332084da951ee4c9c3edd447a5eeb287bbb3886
thoang <thiquynhchi.hoang@ctek.com>
2022-12-28 15:59:16 +0100

    update 11

--- CC2/test_firmware.sh
+++ CC2/test_firmware.sh
@@ -1,21 +1,4 @@
 #!/usr/bin/env bash
-#precheck test staion
-re=$(echo tm fsn|nc -w 20 localhost 8001)
-isJson=$(echo $re | jq -r type)
-if [ $? -ne 0 ]
-then
-    echo 'Test station faulty'
-    exit 1
-fi
-err1=$(echo $re|jq -r '.message.notificationStatus[1].error_code')
-err2=$(echo $re|jq -r '.message.notificationStatus[2].error_code')
-echo 'check error connector 1 $err1'
-echo 'check error connector 2 $err2'
-if [[ $err1 != "NoError" ]] && [[ $err2 != "NoError" ]]
-    then
-        echo "Test station faulty. Fix the issues and try again."
-        exit 1
-fi
 #loop until firmware installation is either finished or failed
 while true
 do
