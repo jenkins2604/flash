7ec81cf297747bcc72ea2f3aaa24a527d077883b
thoang <thiquynhchi.hoang@ctek.com>
2023-01-17 15:40:35 +0100

    update ocl 6

--- CC2/deployOCL.sh
+++ CC2/deployOCL.sh
@@ -7,5 +7,4 @@ sshpass -p root ssh -t root@192.168.7.2 << EOF
   mount | grep -q /usr/share/firmware/ocl_firmware.hex || mount -o bind /mnt/data/ocl_firmware.hex /usr/share/firmware/ocl_firmware.hex
   sleep 3s
   systemctl restart sysmgr
-  sleep 200s
 EOF
