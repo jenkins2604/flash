971be617d5817c93b1147441df2d2b247c8eaf59
thoang <thiquynhchi.hoang@ctek.com>
2023-01-19 15:24:49 +0100

    update 5

--- CC2/updateOCL.sh
+++ CC2/updateOCL.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-
+sshpass -p root scp ocl_firmware.hex root@192.168.7.2:/mnt/data/ocl_firmware.hex
+sshpass -p root scp ocl_firmware.version root@192.168.7.2:/mnt/data/ocl_firmware.version
+sleep 1s
 sshpass -p root ssh -t root@192.168.7.2 << EOF 
-  cat /etc/version
   mount | grep -q /usr/share/firmware/ocl_firmware.version || mount -o bind /mnt/data/ocl_firmware.version /usr/share/firmware/ocl_firmware.version
   mount | grep -q /usr/share/firmware/ocl_firmware.hex || mount -o bind /mnt/data/ocl_firmware.hex /usr/share/firmware/ocl_firmware.hex
   systemctl restart sysmgr
