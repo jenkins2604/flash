a899606783a2f3a73a10155d6e8641c1c40d5793
thoang <thiquynhchi.hoang@ctek.com>
2023-01-20 14:14:51 +0100

    update 6

--- CC2/updateOCL.sh
+++ CC2/updateOCL.sh
@@ -11,7 +11,7 @@ echo loading...
 sleep 200s #make sure that the OCL firmware finish installed, could be improved by checking data received from OCPP
 #Restart OCL by engage Relay3 (unplug power supply to OCL) and then disengage Relay3 (plug power supply to OCL)  then restart CCU
 curl 'http://192.168.17.123/current_state.json?pw=admin&Relay3=1'
-sleep 60s
+sleep 200s
 curl 'http://192.168.17.123/current_state.json?pw=admin&Relay3=0'
 sshpass -p root ssh -t root@192.168.7.2 << EOF 
   systemctl stop ledmgr chargemanager outletmanager rfidcd rfidcd2
