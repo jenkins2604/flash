da26d70ce9d7491d49c218690c7671c28edad331
thoang <thiquynhchi.hoang@ctek.com>
2023-01-24 10:28:03 +0100

    update 1

--- CC2/updateOCL.sh
+++ CC2/updateOCL.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+if [ ! -f "ocl_firmware.hex"  ] ||  [ ! -f "ocl_firmware.version" ]
+then
+	echo "Required files are missing."
+  exit 1
+fi
+
 sshpass -p root scp ocl_firmware.hex root@192.168.7.2:/mnt/data/ocl_firmware.hex
 sshpass -p root scp ocl_firmware.version root@192.168.7.2:/mnt/data/ocl_firmware.version
 sleep 1s
@@ -7,6 +13,7 @@ sshpass -p root ssh -t root@192.168.7.2 << EOF
   mount | grep -q /usr/share/firmware/ocl_firmware.hex || mount -o bind /mnt/data/ocl_firmware.hex /usr/share/firmware/ocl_firmware.hex
   systemctl restart sysmgr
 EOF
+
 echo loading...
 sleep 200s #make sure that the OCL firmware finish installed, could be improved by checking data received from OCPP
 #Restart OCL by engage Relay3 (unplug power supply to OCL) and then disengage Relay3 (plug power supply to OCL)  then restart CCU
@@ -15,6 +22,7 @@ sleep 200s
 curl 'http://192.168.17.123/current_state.json?pw=admin&Relay3=0'
 sshpass -p root ssh -t root@192.168.7.2 << EOF 
   systemctl stop ledmgr chargemanager outletmanager rfidcd rfidcd2
+  sleep 5s
   systemctl restart sysmgr
 EOF
-#sleep 30s
+sleep 30s
