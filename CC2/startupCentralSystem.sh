b096f3263425315cf8afc9ad96339635565c3cff
thoang <thiquynhchi.hoang@ctek.com>
2023-01-24 16:34:45 +0100

    update 2

--- CC2/startupCentralSystem.sh
+++ CC2/startupCentralSystem.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 curl "http://192.168.17.123/current_state.json?pw=admin&SetAll=0" && sleep 1s
-curl "http://192.168.17.123/current_state.json?pw=admin&SetAll=16394" #set Relay4, 2, 5
+curl "http://192.168.17.123/current_state.json?pw=admin&SetAll=16394" #set Relay4, 2, 15
 sleep 50s
 python3 CC2/CentralSystem.py &
 sleep 10s
