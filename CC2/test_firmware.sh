cdb9c92799ac0c68b5a057a395a8aae483bc80f4
thoang <thiquynhchi.hoang@ctek.com>
2023-01-30 13:32:57 +0100

    update 10

--- CC2/test_firmware.sh
+++ CC2/test_firmware.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 while true
 do
-    re=$(echo tm fsn|nc -w 20 localhost 8001)
+    re=$(echo tm fsn|nc -w 5 localhost 8001)
     echo $re
     isJson=$(echo $re | jq -r type)
     if [ $? -ne 0 ]
