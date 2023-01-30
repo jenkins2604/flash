f43390b5d663450b24707b826049cc8b4fd57bdf
thoang <thiquynhchi.hoang@ctek.com>
2023-01-30 14:12:13 +0100

    update 10

--- CC2/updateOCL.sh
+++ CC2/updateOCL.sh
@@ -20,8 +20,8 @@ if [ $? -ne 0 ]; then exit 1; fi
 sshpass -p root scp ocl_firmware.version root@192.168.7.2:/mnt/data/ocl_firmware.version
 if [ $? -ne 0 ]; then exit 1; fi
 sshpass -p root ssh -t root@192.168.7.2 << EOF 
-  mount | grep -q /usr/share/firmware/ocl_firmware.version || mount -o bind /mnt/data/ocl_firmware.version /usr/share/firmware/ocl_firmware.version
-  mount | grep -q /usr/share/firmware/ocl_firmware.hex || mount -o bind /mnt/data/ocl_firmware.hex /usr/share/firmware/ocl_firmware.hex
+  mount -o bind /mnt/data/ocl_firmware.version /usr/share/firmware/ocl_firmware.version
+  mount -o bind /mnt/data/ocl_firmware.hex /usr/share/firmware/ocl_firmware.hex
   systemctl restart sysmgr
 EOF
 
@@ -33,7 +33,7 @@ if [[ $defaultVersion == $OclVersion ]]
     echo 'switch version back to normal'
     echo $(($OclVersion)) > ocl_firmware.version
     sshpass -p root scp ocl_firmware.version root@192.168.7.2:/mnt/data/ocl_firmware.version
-    sshpass -p root ssh root@192.168.7.2 'umount /usr/share/firmware/ocl_firmware.version' 
+    sshpass -p root ssh root@192.168.7.2 'mount -o bind /mnt/data/ocl_firmware.version /usr/share/firmware/ocl_firmware.version' 
     if [ $? -ne 0 ]; then exit 1; fi
     sleep 170s
 else
